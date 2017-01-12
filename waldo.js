27e8f7825a911548edfa4696d6e0ea7de5ccc6d9
joshtran <joshuatrain@gmail.com>
2017-01-12 21:32:50 +0000

    Refactor with proper use of forEach.

--- waldo.js
+++ waldo.js
@@ -1,15 +1,26 @@
 var indexPosition;
 
 function findWaldo(arr, found) {
-  var currentIndex = 0;
-  arr.forEach(function findIndex() {
-    currentIndex++;
-    if (arr[currentIndex] === "Waldo") {
-     indexPosition = currentIndex;
-     found();   // execute callback
+  arr.forEach(
+    function (currentValue, currentIndex) {
+      if (currentValue === "Waldo") {
+        indexPosition = currentIndex;
+        found();
+      }
     }
-  })
+  )
 }
+
+// function findWaldo(arr, found) {
+//   var currentIndex = 0;
+//   arr.forEach(function findIndex() {
+//     currentIndex++;
+//     if (arr[currentIndex] === "Waldo") {
+//      indexPosition = currentIndex;
+//      found();   // execute callback
+//     }
+//   })
+// }
 
 function actionWhenFound() {
   console.log("Found him at index " + indexPosition + "!");
